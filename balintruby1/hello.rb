134a8b5f2829f4ffc0c3d390442587514cbede05
Balint Gaspar <balintdevedu@hunglish.com>
2017-09-27 20:31:37 -0400

    updated 1st ruby file

---
+++ balintruby1/hello.rb
@@ -0,0 +1,46 @@
+puts "Starting our program... \"To infinity and beyond\""
+
+var1 = ARGV[0].to_i
+# var2 = ARGV[1].to_i
+
+
+# unless var1 == 2
+#     puts "It is not 2"
+# else
+#     puts "I don't know"
+# end
+
+# case var1
+# when 1 || 2
+#     puts "It is 1 or 2"
+# when 3
+#     puts "It is 3"
+# when 4
+#     puts "It is 4"
+# else
+#     puts "I don't know what it is"
+# end
+
+
+# && => AND
+# || => OR
+
+# if var1 == 5 && var2 == 7
+#     puts "they are correct"
+# elsif var1 == "5"
+#     puts "Pretty close"
+# else
+#     puts "no dice"
+# end
+
+#puts "var1: #{var1}"
+#puts "var2: #{var2}"
+
+#if cool_number == 41
+#    puts "Number is 41"
+#elsif cool_number == 42
+#    puts "Number is NOT 41"
+#else
+#    puts "How did I get here?"
+#end
+
