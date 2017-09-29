299c5984357837287ef4645ad251c182758ae3cc
Stephanie Rockett <stephanie_rockett@yahoo.com>
2017-09-29 18:58:11 -0400

    ruby class stuff

---
+++ stephanie/hello.rb
@@ -0,0 +1,42 @@
+#puts "your name here"
+
+var1 = ARGV[0].to_i
+# var2 = ARGV[1]
+
+
+
+# unless var1 == 2
+#     puts "it is 2"
+# else
+#     puts "i don't know"
+# end
+
+# case var1
+#     when 1 || 2 
+#         puts "it is 1 or 2"
+#     when 3
+#         puts "it is 3"
+#     when 4 
+#         puts "it is 4"
+#     else
+#         puts "don't know what it is"
+# end
+
+# if var1 == "5" && var2 == "7"
+#     puts "they are correct"
+# elsif var1 == "5"
+#     puts "pretty close"
+# else
+#     puts "no dice"
+# end
+
+# puts "var1: #{var1}"
+# puts "var2: #{var2}"
+
+# if cool_number >= 41
+#     puts "Number is 41"
+# elsif cool_number == 42
+#     puts "Number is not 41"
+# else
+#     puts "How did I get here?" 
+# end
---
+++ stephanie/loops.rb
@@ -0,0 +1,37 @@
+# Loops
+
+# var_cat_count = ARGV[0].to_i
+
+# puts var_cat_count.to_s
+# running_count = 0
+
+# while running_count < var_cat_count
+#     running_count += 1
+#     puts "I have #{running_count.to_s} cats"
+# end
+
+# people = ["Stephanie", "Balint", "Taylor", "Joseph"]
+
+# people.each_with_index do |person, index|
+#     puts "#{person} is number #{index}"
+# end
+
+# puts "Person at index 2: " + people[1]
+
+### arrays
+our_arrays =[]
+
+### hashes
+our_hash = {}
+
+our_hash["food"] = "steak"
+our_hash["drink"] = "beer"
+
+puts our_hash["drink"]
+
+
+### JSON
+# {
+#     "key1": "value1"
+#     "key2": "value2"
+# }
